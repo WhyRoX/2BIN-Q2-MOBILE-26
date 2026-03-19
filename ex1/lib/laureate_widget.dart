8013a60dfe2823f9f42031dc32f7a9163dd6d851
WhyRoX <enes.12.yumer@gmail.com>
2026-03-19 15:41:09 +0100

    F01.3 stateless widget peaufiné

---
+++ ex1/lib/laureate_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class LaureateWidget extends StatelessWidget {
+  final String motivation;
+  final String firstname;
+  final String surname;
+
+  const LaureateWidget({
+    super.key,
+    required this.motivation,
+    this.firstname = "",
+    this.surname = "",
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(motivation, textAlign: TextAlign.justify),
+          ),
+          Card(
+            color: Theme.of(context).colorScheme.inverseSurface,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "$firstname $surname",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onInverseSurface,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ ex1/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import 'laureate_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Exercice 1',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Exercice 1"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            LaureateWidget(
+              motivation: laureate1["motivation"]!,
+              firstname: laureate1["firstname"]!,
+              surname: laureate1["surname"]!,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+const laureate1 = {
+  "id": "819",
+  "firstname": "Al",
+  "surname": "Gore",
+  "motivation":
+      "For their efforts to build up and disseminate greater knowledge about man-made climate change, and to lay the foundations for the measures that are needed to counteract such change.",
+  "share": "2",
+};
