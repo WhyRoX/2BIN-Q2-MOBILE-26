76d21fea96d74f632affafbf3e650cf1936805c3
WhyRoX <enes.12.yumer@gmail.com>
2026-03-26 16:09:38 +0100

    T04.6 View model provider

---
+++ tuto4/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:tuto4/user_screen.dart';
+import 'click_view_model.dart';
+import 'second_screen.dart';
+import 'first_screen.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
+
+final _router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const FirstScreen(),
+      routes: [
+        GoRoute(
+          path: 'second',
+          builder: (context, state) {
+            final nbClicks = Provider.of<ClickViewModel>(
+              context,
+              listen: false,
+            ).clicks;
+            return SecondScreen(nbClicks: nbClicks);
+          },
+        ),
+        GoRoute(
+          path: 'users/:username',
+          builder: (context, state) =>
+              UserScreen(username: state.pathParameters['username'] ?? ''),
+        ),
+      ],
+    ),
+  ],
+);
+
+void main() {
+  usePathUrlStrategy();
+  runApp(
+    ChangeNotifierProvider<ClickViewModel>(
+      create: (context) => ClickViewModel(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: _router,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
